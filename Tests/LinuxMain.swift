fe8f34e5641a2e0867045828e605483fae93319c
Azmi Muhammad <m.azmi65@yahoo.com>
2021-07-13 11:32:52 +0700

    adding SPM

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import NetworkLibsCombineTests
+
+var tests = [XCTestCaseEntry]()
+tests += NetworkLibsCombineTests.allTests()
+XCTMain(tests)
---
+++ Tests/NetworkLibsCombineTests/NetworkLibsCombineTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import NetworkLibsCombine
+
+final class NetworkLibsCombineTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NetworkLibsCombine().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
